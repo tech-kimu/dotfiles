b1b995438bcbc412270d33c2574044be6bbff228
Kangsan Kim <tech.kimu@gmail.com>
2026-06-18 23:14:28 +0900

    No message

---
+++ nvim/init.lua
@@ -0,0 +1,11 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.swapfile = false
+
+--vim.pack.add(
+--)
+
+vim.cmd.colorscheme("molokai")
+--vim.cmd.highlight("statusline guibg=NONE")
