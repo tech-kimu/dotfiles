a7bcbf848dd7ab87b3ee2c8b3cc1935e1a527eec
Kangsan Kim <tech.kimu@gmail.com>
2026-07-06 22:56:37 +0900

    Updated

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -6,12 +6,21 @@ require("input")
 
 hl.config({
 	general = {
-		border_size = 2,
+		border_size = 0,
 		gaps_in = 5,
 		gaps_out = 10,
 		resize_on_border = true,
 		layout = "dwindle",
 	},
+	decoration = {
+        rounding = 10,
+        rounding_power = 2,
+		dim_inactive = true,
+		dim_strength = 0.4
+	},
+    animations = {
+        enabled = true,
+    },
     misc = {
         disable_hyprland_logo = true,
 		disable_splash_rendering = true,
@@ -37,7 +46,6 @@ hl.config({
 hl.env("XCURSOR_SIZE", "24")
 hl.env("HYPRCURSOR_SIZE", "24")
 
-
 -----------------------
 ----- PERMISSIONS -----
 -----------------------
@@ -60,43 +68,6 @@ hl.env("HYPRCURSOR_SIZE", "24")
 -----------------------
 ---- LOOK AND FEEL ----
 -----------------------
-
--- Refer to https://wiki.hypr.land/Configuring/Basics/Variables/
-hl.config({
-    general = {
-        col = {
-            active_border   = { colors = {"rgba(33ccffee)", "rgba(00ff99ee)"}, angle = 45 },
-            inactive_border = "rgba(595959aa)",
-        },
-    },
-
-    decoration = {
-        rounding       = 10,
-        rounding_power = 2,
-
-        -- Change transparency of focused and unfocused windows
-        active_opacity   = 1.0,
-        inactive_opacity = 1.0,
-
-        shadow = {
-            enabled      = true,
-            range        = 4,
-            render_power = 3,
-            color        = 0xee1a1a1a,
-        },
-
-        blur = {
-            enabled   = true,
-            size      = 3,
-            passes    = 1,
-            vibrancy  = 0.1696,
-        },
-    },
-
-    animations = {
-        enabled = true,
-    },
-})
 
 -- Default curves and animations, see https://wiki.hypr.land/Configuring/Advanced-and-Cool/Animations/
 hl.curve("easeOutQuint",   { type = "bezier", points = { {0.23, 1},    {0.32, 1}    } })
